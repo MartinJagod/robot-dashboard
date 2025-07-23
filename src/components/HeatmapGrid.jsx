f94c011c228506c219a269aac296c04e8469100f
martinJagod <jagodnikmartin@gmail.com>
2025-07-23 11:26:01 -0300

    Cambie la escala de referencias

--- src/components/HeatmapGrid.jsx
+++ src/components/HeatmapGrid.jsx
@@ -6,67 +6,39 @@ import { faHouse }        from '@fortawesome/free-solid-svg-icons';
 export const getColorForValue = (value, type) => {
   if (type === 'temperature') {
   // Escala basada en la imagen adjunta
-  if (value < 65)      return '#153E89';  // Below 65 °F
-  if (value < 68)      return '#25780D';  // Below 65 °F
-
-  if (value <= 75)     return '#00B227';  // 68 – 74 °F
-  if (value <= 82)     return '#ffc300';  // 75 – 82 °F
-  if (value <= 88)     return '#FE3103';  // 82 – 88 °F
-  if (value <= 93)     return '#9A0000';  // 88 – 93 °F
+  if (value >= 93)     return '#9A0000';  // 88 – 93 °F
+  if (value >= 88)     return '#FE3103';  // 82 – 88 °F
+  if (value >= 82)     return '#ffc300';  // 75 – 82 °F
+  if (value >= 75)     return '#00B227';  // 68 – 74 °F
+  if (value >= 68)      return '#25780D';  // Below 65 °F
+  if (value < 68)      return '#153E89';  // Below 65 °F
   /* > 93 °F */
   return '#9A0000';                       // Above 93 °F
 }
 if (type === 'humidity') {
   // Escala de la imagen adjunta
+  if (value >= 80)     return '#021840'; // 70 – 80 %
+  if (value >= 70)     return '#153E89'; // 60 – 70 %
+  if (value >= 60)     return '#256FCF'; // 55 – 60 %
+  if (value >= 55)     return '#8A30CF'; // 50 – 55 %
+  if (value >= 50)     return '#D679E4'; // 45 – 50 %
+  if (value >= 45)     return '#3E8E2E'; // 40 – 45 %
+  if (value >= 40)     return '#8FC588'; // 30 – 40 %
+  if (value >= 20)     return '#989898'; // 20 – 30 %
   if (value < 20)      return '#C9C9C9'; // < 20 %
-  if (value <= 30)     return '#989898'; // 20 – 30 %
-  if (value <= 40)     return '#8FC588'; // 30 – 40 %
-  if (value <= 45)     return '#3E8E2E'; // 40 – 45 %
-  if (value <= 50)     return '#D679E4'; // 45 – 50 %
-  if (value <= 55)     return '#8A30CF'; // 50 – 55 %
-  if (value <= 60)     return '#256FCF'; // 55 – 60 %
-  if (value <= 70)     return '#153E89'; // 60 – 70 %
-  if (value <= 80)     return '#021840'; // 70 – 80 %
   /* > 80 % */
   return '#021840';
 }
 
-/*   if (type === 'temperature') {
-
-    if (value <= 64) return '#696969';
-    if (value <= 72) return '#2121cf';
-    if (value <= 76) return '#0092ff';
-    if (value <= 81) return '#00abb2';
-    if (value <= 85) return '#209000';
-    if (value <= 90) return '#ffc300';
-    if (value <= 95) return '#fe3103';
-    if (value <= 100 ) return '#9a0000';
-
-    return '#9a0000';
-  } */
-/*   if (type === 'humidity') {
-    if (value <= 20) return '#f0f0f0';
-    if (value <= 30) return '#ccd8c9';
-    if (value <= 40) return '#acf2a3';
-    if (value <= 45) return '#78e4d4';
-    if (value <= 50) return '#8eddff';
-    if (value <= 55) return '#b7abfb';
-    if (value <= 60) return '#6e70ea';
-    if (value <= 70) return '#53039a';
-    if (value <= 80) return '#38004e';
-    return '#38004e';
-  } */
   if (type === 'combined') {
-    if (value < 83.5) return '#696969';
-    if (value < 85) return '#2121cf';
-    if (value < 88.5) return '#0092ff';
-    if (value < 89.5) return '#00abb2';
-    if (value < 91) return '#209000';
-    if (value < 92.5) return '#ffc300';
-    if (value < 94) return '#fe3103';
-    if (value < 98 ) return '#9a0000';
-
-    return '#9a0000';
+  if (value < 65)      return '#153E89';  // Below 65 °F
+  if (value < 68)      return '#25780D';  // Below 65 °F
+  if (value <= 75)     return '#00B227';  // 68 – 74 °F
+  if (value <= 82)     return '#ffc300';  // 75 – 82 °F
+  if (value <= 88)     return '#FE3103';  // 82 – 88 °F
+  if (value <= 93)     return '#9A0000';  // 88 – 93 °F
+  /* > 93 °F */
+  return '#9A0000'; 
   }
   return '#ffffff';
 };
